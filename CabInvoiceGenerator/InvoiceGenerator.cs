67de950495c803e632add33ef1bcecf4da98678c
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-18 17:48:21 +0530

    [vijayalakshmi] calculate distance and invoice generate total fare for journey

--- CabInvoiceGenerator/InvoiceGenerator.cs
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -8,47 +8,36 @@ namespace CabInvoiceGenerator
 {
     public class InvoiceGenerator
     {
-        private readonly double MINIMUM_COST_PER_KM = 10;
-        private readonly int COST_PER_MINUTE = 1;
-        private readonly double MINIMUM_FARE = 5;
-        public double distance;
-        public int time;
-        public double CalculateFare(Ride ride)
-        {
-            double totalFare = 0;
-            if (ride.distance >= 0 && ride.time >= 0)
+            private readonly double MINIMUM_COST_PER_KM = 10;
+            private readonly int COST_PER_MINUTE = 1;
+            private readonly double MINIMUM_FARE = 5;
+            public double distance;
+            public int time;
+            public InvoiceGenerator(double distance, int time)
             {
-                totalFare = ride.distance * MINIMUM_COST_PER_KM + ride.time * COST_PER_MINUTE;
+                this.distance = distance;
+                this.time = time;
             }
-            else
+            public double TotalFare()
             {
-                if (ride.distance <= 0)
+                double totalFare = 0;
+                if (distance >= 0 && time >= 0)
                 {
-                    throw new CustomException(CustomException.ExceptionType.INVALID_DISTANCE, "Invalid Distance");
+                    totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_MINUTE;
                 }
-                if (ride.time < 0)
+                else
                 {
-                    throw new CustomException(CustomException.ExceptionType.INVALID_TIME, "Invalid Time");
+                    if (distance <= 0)
+                    {
+                        throw new CustomException(CustomException.ExceptionType.INVALID_DISTANCE, "Invalid Distance");
+                    }
+                    if (time < 0)
+                    {
+                        throw new CustomException(CustomException.ExceptionType.INVALID_TIME, "Invalid Time");
+                    }
                 }
+                return totalFare;
             }
-            return Math.Max(totalFare, MINIMUM_FARE);
-        }
-        public EnhanceInvoice MultipleRides(Ride[] rides)
-        {
-            double totalFare = 0;
-            try
-            {
-                foreach (var ride in rides)
-                {
-                    totalFare += CalculateFare(ride);
-                }
-            }
-            catch (InvoiceGeneratorExceptions)
-            {
-                throw new InvoiceGeneratorExceptions(InvoiceGeneratorExceptions.ExceptionType.NULL_RIDES, "Rides Are Null");
-            }
-            return new(rides.Length, totalFare);
-        }
-
+        
     }
 }
--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -8,21 +8,13 @@ namespace CabInvoiceGenerator
     {
         public static void Main(string[] args)
         {
-            /* Console.WriteLine("Enter distance you want to Ride");
-             int input1 = Convert.ToInt32(Console.ReadLine());
-             Console.WriteLine("Enter time of your journey in minutes");
-             int input2 = Convert.ToInt32(Console.ReadLine());
-             InvoiceGenerator getMethod = new InvoiceGenerator(input1,input2);
-             double fare = getMethod.TotalFare();
-             Console.WriteLine("Fare: " + fare);
-            
-            Ride[] ride = { new Ride(10, 10), new Ride(10, 5) };
-            InvoiceGenerator getMethod = new InvoiceGenerator();
-            double fare = getMethod.MultipleRides(ride); */
-            InvoiceGenerator name = new InvoiceGenerator();
-            Ride[] ride = { new Ride(20, 20), new Ride(10, 5), new Ride(30, 30) };
-            EnhanceInvoice invoice = name.MultipleRides(ride);
-            Console.WriteLine("TotalFare: " + invoice.totalFare + "\nNumberOfRides: " + invoice.numberOfRides + "\nAverage Fare: " + invoice.averageFare);
+            Console.WriteLine("Enter distance you want to Ride");
+            double input1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter time of your journey in minutes");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+            InvoiceGenerator getMethod = new InvoiceGenerator(input1, input2);
+            double fare = getMethod.TotalFare();
+            Console.WriteLine("Fare: " + fare);
         }
     }
 
